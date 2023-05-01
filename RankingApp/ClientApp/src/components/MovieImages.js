0ab5d365ec8bac700de2eb96d22dff97d505526c
Kalyb Levesque <kalyb@keystrokesinc.com>
2023-05-01 16:29:52 -0500

    first commit

---
+++ RankingApp/ClientApp/src/components/MovieImages.js
@@ -0,0 +1,26 @@
+﻿import Godfather from '../images/Movies/Godfather.png';
+import Highlander from '../images/Movies/Highlander.png';
+import Highlander2 from '../images/Movies/Highlander2.png';
+import LastOfTheMohicans from '../images/Movies/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../images/Movies/PoliceAcademy6.png';
+import RearWindow from '../images/Movies/RearWindow.png';
+import RoadHouse from '../images/Movies/RoadHouse.png';
+import Shawshank from '../images/Movies/Shawshank.png';
+import StarTrack4 from '../images/Movies/StarTreck4.png';
+import Superman4 from '../images/Movies/Superman4.png';
+
+const MovieImageArr = [
+    { id: 1, image: Godfather },
+    { id: 2, image: Highlander },
+    { id: 3, image: Highlander2 },
+    { id: 4, image: LastOfTheMohicans },
+    { id: 5, image: PoliceAcademy6 },
+    { id: 6, image: RearWindow },
+    { id: 7, image: RoadHouse },
+    { id: 8, image: Shawshank },
+    { id: 9, image: StarTrack4 },
+    { id: 10, image: Superman4 },
+
+];
+
+export default MovieImageArr;
